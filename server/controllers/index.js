1b478dec70a86273379ed5aaf9c105f6319e7664
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-05-05 12:55:48 +0100

    feature(index.js): export endpoints files
    For create, list, retrieve and update functions
    [#144926557, #144926531, #144926549, #144926441]

---
+++ server/controllers/index.js
@@ -0,0 +1,11 @@
+const roles = require('./role');
+const folder = require('./folder');
+const documents = require('./documents');
+const users = require('./users');
+
+module.exports = {
+  roles,
+  folder,
+  documents,
+  users
+};
