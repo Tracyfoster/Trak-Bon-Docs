3384e951167d364d066c993dfc384b4a02e013b0
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-05-05 13:51:05 +0100

    feature(app.js, index.js, index.html): create app entry point

---
+++ client/components/App.js
@@ -0,0 +1,15 @@
+/** jsx */
+import React, { Component } from 'react';
+import MainLayout from '../Pages/MainLayout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainLayout />
+      </div>
+    );
+  }
+}
+
+export default App;
