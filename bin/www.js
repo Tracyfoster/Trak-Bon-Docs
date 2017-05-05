40f172f172515af9e8106bf2c9d0a5b3d2030dde
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-05-05 12:37:59 +0100

    chore(www.js, app.js): update app.js and initialize with www.js

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,38 @@
-const http = require('http');
-const app = require('../app');
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from '../webpack.config.dev';
+import open from 'open';
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-app.set('port', port);
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(app);
-server.listen(port);
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../client/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
