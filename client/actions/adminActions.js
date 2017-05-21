d43d7dc1bd23dc618758daf7015e8856748c6d50
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-05-21 05:02:15 +0100

    feature(adminActions): modify file to include some filters

--- client/actions/adminActions.js
+++ client/actions/adminActions.js
@@ -1,28 +1,44 @@
 import axios from 'axios';
-import { SET_USERS, USER_FETCHED, USER_UPDATED, USER_DELETED } from './types';
+import * as types from './types';
+import { eventAction } from '../utils/Utils';
 
-const eventAction = (type, payload) => ({
-  type,
-  payload
-});
+export const fetchUsers = () => dispatch =>
+  axios.get('/api/users')
+    .then((res) => {
+      const users = {};
+      const payload = {};
+      users.data = res.data;
+      users.adminCount = res.data.filter(user => user.roleId === 1);
+      users.reviewersCount = res.data.filter(user => user.roleId === 2);
+      users.writersCount = res.data.filter(user => user.roleId === 3);
+      payload.users = users;
+      return dispatch(eventAction(types.SET_USERS, payload));
+    });
 
-export function fetchUsers() {
-  return dispatch => axios.get('/api/users')
-      .then(res => res.data)
-      .then(data => dispatch(eventAction(SET_USERS, data)));
-}
+export const fetchUser = id => dispatch =>
+  axios.get(`/api/users/${id}`)
+    .then(res => dispatch(eventAction(types.USER_FETCHED, res.data)));
 
-export function fetchUser(id) {
-  return dispatch => axios.get(`/api/users/${id}`)
-      .then(res => dispatch(eventAction(USER_FETCHED, res.data)));
-}
+export const updateUser = user => dispatch =>
+  axios.put(`/api/users/${user.id}`, user)
+    .then(res => dispatch(eventAction(types.USER_UPDATED, res.data)));
 
-export function updateUser(user) {
-  return dispatch => axios.put(`/api/users/${user.id}`, user)
-      .then(res => dispatch(eventAction(USER_UPDATED, res.data)));
-}
-export function deleteUser(id) {
-  return dispatch => axios.delete(`/api/users/${id}`)
-      .then(res => res.data)
-      .then(data => dispatch(eventAction(id)));
-}
+export const deleteUser = id => dispatch =>
+    axios.delete(`/api/users/${id}`)
+      .then(res => dispatch(eventAction(types.USER_DELETED, res.data)));
+
+export const fetchUserDocuments = id => dispatch =>
+    axios.get(`/api/users/${id}/documents`)
+      .then((res) => {
+        const userDocuments = {};
+        const payload = {};
+        userDocuments.data = res.data.doc;
+        userDocuments.public = (res.data.doc
+          .filter(document => document.access === 'public'));
+        userDocuments.private = (res.data.doc
+          .filter(document => document.access === 'private'));
+        userDocuments.role = (res.data.doc
+          .filter(document => document.access === 'role'));
+        payload.userDocuments = userDocuments;
+        return dispatch(eventAction(types.SET_USER_DOCUMENTS, payload));
+      });
