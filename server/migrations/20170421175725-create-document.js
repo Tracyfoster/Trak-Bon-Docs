32f1d057618f16d71e72cdf46a26bac9972420ba
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-06-08 12:19:05 +0100

    change migrate folder to migrations

---
+++ server/migrations/20170421175725-create-document.js
@@ -0,0 +1,60 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Documents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      access: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [['public', 'private', 'writers', 'reviewers']],
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        },
+      },
+      folderId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Folder',
+          key: 'id',
+          as: 'folderId',
+        },
+      },
+    }),
+  down: queryInterface =>
+    queryInterface.dropTable('Documents')
+};
