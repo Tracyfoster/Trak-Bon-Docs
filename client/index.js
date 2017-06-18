3bf7bf9e3415027a194171bd8fe5d6cf6403e1c7
andela-tezebuike <tracy.ezebuike@andela.com>
2017-06-18 18:27:21 +0100

    feature(components): rename files to jsx
    [#143671477]

--- client/index.js
+++
@@ -1,7 +0,0 @@
-import 'react-mdl/extra/material';
-import 'react-mdl/extra/material.css';
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-//
-render(<App />, document.getElementById('app'));
